3d7e8707abe03bdfdfe6c48775020db4e40724a1
dpilafian <dem@centerkey.com>
2022-11-17 02:58:40 -0800

    Initial version

---
+++ cli-argv-util.ts
@@ -0,0 +1,36 @@
+// cli-argv-util ~~ MIT License
+
+export type StringFlagMap =  { [flag: string]: string | undefined };
+export type BooleanFlagMap = { [flag: string]: boolean };
+export type Result = {
+   flagMap:        StringFlagMap,   //map of flag values for each user supplied flag
+   flagOn:         BooleanFlagMap,  //map of the enabled status for all valid flags
+   invalidFlag:    string | null,   //name of the first invalid flag
+   invalidFlagMsg: string | null,   //error message for the invalid flag
+   params:         string[],        //array of parameter values supplied by the user
+   };
+
+const cliArgvUtil = {
+
+   parse(validFlags: string[]): Result {
+      const toCamel =     (token: string) =>  token.replace(/-./g, char => char[1]!.toUpperCase());  //example: 'no-map' --> 'noMap'
+      const toEntry =     (pair: string[]) => [toCamel(pair[0]!), pair[1]];      //example: ['no-map'] --> ['noMap', undefined]
+      const toPair =      (flag: string) =>   flag.replace(/^--/, '').split('=');  //example: '--cd=build' --> ['cd', 'build']
+      const args =        process.argv.slice(2);
+      const pairs =       args.filter(arg => /^--/.test(arg)).map(toPair);
+      const flagMap =     Object.fromEntries(pairs.map(toEntry));
+      const onEntries =   validFlags.map(flag => [toCamel(flag), toCamel(flag) in flagMap]);
+      const invalidFlag = pairs.find(pair => !validFlags.includes(pair[0]!))?.[0] ?? null;
+      const helpMsg =     '\nValid flags are --' + validFlags.join(' --');
+      return {
+         flagMap:        flagMap,
+         flagOn:         Object.fromEntries(onEntries),
+         invalidFlag:    invalidFlag,
+         invalidFlagMsg: invalidFlag ? 'Invalid flag: --' + invalidFlag + helpMsg : null,
+         params:         args.filter((arg: string) => !/^--/.test(arg)),
+         };
+      },
+
+   };
+
+export { cliArgvUtil };
---
+++ spec/mocha.spec.js
@@ -0,0 +1,133 @@
+// cli-argv-util
+// Mocha Specification Suite
+
+// Imports
+import { assertDeepStrictEqual } from 'assert-deep-strict-equal';
+import fs from 'fs';
+
+// Setup
+import { cliArgvUtil } from '../dist/cli-argv-util.js';
+const mockCli = (line) => process.argv = ['node', 'mock.js', ...line.split(' ')];
+
+////////////////////////////////////////////////////////////////////////////////
+describe('The "dist" folder', () => {
+
+   it('contains the correct files', () => {
+      const actual = fs.readdirSync('dist').sort();
+      const expected = [
+         'cli-argv-util.d.ts',
+         'cli-argv-util.js',
+         'cli-argv-util.umd.cjs',
+         ];
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////
+describe('Library module', () => {
+
+   it('is an object', () => {
+      const actual =   { constructor: cliArgvUtil.constructor.name };
+      const expected = { constructor: 'Object' };
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   it('has a parse() function', () => {
+      const actual =   { validate: typeof cliArgvUtil.parse };
+      const expected = { validate: 'function' };
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////
+describe('Calling cliArgvUtil.parse()', () => {
+
+   it('results in the correct CLI data', () => {
+      const validFlags = ['flag1', 'flag2', 'flag3'];
+      mockCli('file.html --flag1 file.png --flag3=three');
+      const actual = cliArgvUtil.parse(validFlags);
+      const expected = {
+         flagMap: {
+            flag1: undefined,
+            flag3: 'three',
+            },
+         flagOn: {
+            flag1: true,
+            flag2: false,
+            flag3: true,
+            },
+         invalidFlag:    null,
+         invalidFlagMsg: null,
+         params:         ['file.html', 'file.png'],
+         };
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   it('with kebab flags results in the correct CLI data', () => {
+      const validFlags = ['flag-one', 'flag-two', 'flag-three'];
+      mockCli('file.html --flag-one file.png --flag-three=three');
+      const actual = cliArgvUtil.parse(validFlags);
+      const expected = {
+         flagMap: {
+            flagOne:   undefined,
+            flagThree: 'three',
+            },
+         flagOn: {
+            flagOne:   true,
+            flagTwo:   false,
+            flagThree: true,
+            },
+         invalidFlag:    null,
+         invalidFlagMsg: null,
+         params:         ['file.html', 'file.png'],
+         };
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   it('with no flags or parameters results in the correct CLI data', () => {
+      const validFlags = ['flag1', 'flag2', 'flag3'];
+      process.argv = ['node', 'mock.js'];
+      const actual = cliArgvUtil.parse(validFlags);
+      const expected = {
+         flagMap: {},
+         flagOn: {
+            flag1: false,
+            flag2: false,
+            flag3: false,
+            },
+         invalidFlag:    null,
+         invalidFlagMsg: null,
+         params:         [],
+         };
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////
+describe('Correct error message is generated', () => {
+
+   it('when a bogus flag is provided', () => {
+      const validFlags = ['flag1', 'flag2', 'flag3'];
+      mockCli('file.html --bogus file.png --flag3=three');
+      const actual = cliArgvUtil.parse(validFlags);
+      const expected = {
+         flagMap: {
+            bogus: undefined,
+            flag3: 'three',
+            },
+         flagOn: {
+            flag1: false,
+            flag2: false,
+            flag3: true,
+            },
+         invalidFlag:    'bogus',
+         invalidFlagMsg: 'Invalid flag: --bogus\nValid flags are --flag1 --flag2 --flag3',
+         params:         ['file.html', 'file.png'],
+         };
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   });
